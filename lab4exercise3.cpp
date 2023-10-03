2275187e67dd86e9912b7618e90f766ac7d975fa
sqalli <tahrisqalli@gmail.com>
2023-10-03 10:43:39 +0400

    Update lab4exercise3.cpp

--- lab4exercise3.cpp
+++ lab4exercise3.cpp
@@ -1,4 +1,4 @@
-/*Implement the Employee and derived classes as shown in images/exercise3.png.
+/*Implement the Employee and derived classes.
 In the Employee hierarchy, getPay() is implemented differently in each subcla-
 ss and must be a virtual method. Provide implementation for getPay() for each 
 derived class.*/
